b5f5ddd7ea15e5499affa1a8b6f31d9df66774c9
RuthieXIT <rignacio.prof@gmail.com>
2024-03-03 23:19:54 +0800

    Submit-changes

--- payag-ni-jose/submit-form.php
+++ payag-ni-jose/submit-form.php
@@ -1,3 +1,30 @@
-<?php
-echo "Thanks for your Submission!"
-?>
+
+<!DOCTYPE html>
+<html lang ="en">
+            <head>
+                <title> Payag ni Jose</title>
+                <meta charset="utf-8"> 
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                
+            </head>
+    <body>
+            <header> <h1> Hello </h></header>
+            <main>
+           
+             <?php
+                echo "Thank you for your submission!"
+                $firstName =$_POST['first_name'];
+
+                if(isset($firstName)) {echo '<p><b> First Name: </b>' . $firstName . '</.p>';}
+
+
+             ?>
+
+            </main>
+
+
+
+
+    </body>
+
+</html>
